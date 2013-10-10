5b408e7f8035e38df7ff43675935dd2d83ffac05
wangjohn <wangjohn@mit.edu>
2013-10-09 23:30:05 -0500

    Adding tree page model.

---
+++ app/models/tree_page.js
@@ -0,0 +1,18 @@
+/*
+ * TreePage
+ *
+ * This is the model that actually stores the html for a particular url.
+ *
+ */
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var TreePageSchema = mongoose.Schema({
+  treeUrl: { type: String, required: true, trim: true },
+  treeHtml: { type: String, required: true, trim: true }
+});
+
+var TreePage = mongoose.model('TreePage', TreePageSchema);
+
+exports.TreePage = TreePage;
