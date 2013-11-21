f2562bc43a7a6b56fa8b853c678ce39ee5273a67
Ted Benson <edward.benson@gmail.com>
2013-11-20 22:31:12 -0500

    added bookmarklet links to index, removed jumbotron

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -21,6 +21,7 @@ def update_treesheets():
   with cd('/sites/treesheets.org/code'):
     run('git pull origin master')
     run('git submodule update')
+    run('git submodule -q foreach git pull -q origin master')
 
 def refresh_cts():
   """Refreshes the local CTS and CTSUI files."""
@@ -30,13 +31,33 @@ def refresh_cts():
   local('cp ../../cts-ui/release/cts-ui.js ../static/hotlink/cts-ui.js')
   local('git add ../static/hotlink/cts.js')
   local('git add ../static/hotlink/cts-ui.js')
-  local('git commit -m "refreshing CTS JS and UI"')
+  local('git commit -m "refreshing CTS JS and UI [fabfile]"')
   local('git push origin master')
 
 def deploy():
   """Updates git and restarts."""
   update_treesheets()
   restart_treesheets()
+
+def push_mockups():
+  """Copies mockups from cts-ui to mockups package, commits, and pushes"""
+  local('cd ../../cts-ui && grunt')
+  local('cp ../../cts-ui/mockups/css/*.css ../../mockups/cts-ui/css/.')
+  local('cp -R ../../cts-ui/mockups/css/bootstrap ../../mockups/cts-ui/css/bootstrap')
+  local('cp -R ../../cts-ui/mockups/img ../../mockups/cts-ui/img')
+  local('cp ../../cts-ui/mockups/*.html ../../mockups/cts-ui/.')
+  local('cd ../../mockups/cts-ui && git add *.html')
+  local('cd ../../mockups/cts-ui/css && git add *.css')
+  local('cd ../../mockups/cts-ui/css && git add bootstrap/*')
+  local('cd ../../mockups/cts-ui && git add img/*')
+  local('cd ../../mockups && git commit -am "New cts-ui mockups [fabfile]"')
+  local('cd ../../mockups && git push origin master')
+
+def full_deploy():
+  """Refresh, copy mockups, deploy"""
+  refresh_cts()
+  push_mockups()
+  deploy()
 
 def main():
   print """
